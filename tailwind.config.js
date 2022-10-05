c25612be91177047ecaf5948fb55167221b74f77
Niko <45226198+nikoprl@users.noreply.github.com>
2022-10-05 22:09:44 +0300

    Update footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
         "white": "#FFFFFF",
         "emerald": "#44CF6C",
         "middlepurple": "#7D82B8",
+      },
+      backgroundImage: {
+        'footer-texture': "url(/src/image/layered-waves-bg.svg)"
+      },
+      dropShadow: {
+        'footer-shadow': "0px -12px 23px rgba(0, 0, 0, 0.4)"
       }
     },
   },
